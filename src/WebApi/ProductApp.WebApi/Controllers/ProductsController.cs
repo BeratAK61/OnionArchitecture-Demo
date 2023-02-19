53aaca4fb77c19d099c2f3dfe389bae270cac743
Berat AK <brt_ak_61@hotmail.com>
2023-02-19 14:50:26 +0300

    Add ServiceRegistration Classes

--- src/WebApi/ProductApp.WebApi/Controllers/ProductsController.cs
+++ src/WebApi/ProductApp.WebApi/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.DTOs;
 using ProductApp.Application.Interfaces.Repository;
 
 namespace ProductApp.WebApi.Controllers
@@ -8,6 +9,25 @@ namespace ProductApp.WebApi.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        IProductRepository _productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allList = await _productRepository.GetAllAsync();
+
+            var result = allList.Select(i => new ProductViewDto()
+            {
+                Id = i.Id,
+                Name = i.Name,
+            }).ToList();
+
+            return Ok(result);  
+        }
     }
 }
+
