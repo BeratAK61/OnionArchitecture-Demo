1aa6ebda9865faae5630aa3cc6d8f460e35938de
Berat AK <brt_ak_61@hotmail.com>
2023-02-19 03:27:17 +0300

    Add project files.

---
+++ src/Core/ProductApp.Application/Interfaces/Repository/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Interfaces.Repository
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+    }
+}
